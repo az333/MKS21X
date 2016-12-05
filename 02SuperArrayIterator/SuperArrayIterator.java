7c0cd6412dbb7bd6e34cc29d2b966c1c5fe6b2df
Angelica Zverovich <azverovich@stuy.edu>
2016-12-05 12:07:08 -0500

    SuperArray Iterators

---
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,30 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SuperArrayIterator implements Iterator<String>{
+    private int element;
+    private SuperArray supa;
+
+    public SuperArrayIterator(SuperArray a){
+	supa = a;
+	element=0;
+    }
+    public boolean hasNext(){
+	return element < supa.size();
+    }
+    public String next(){
+	if (hasNext()){
+	    element++;
+	    return supa.get(element-1);
+	}
+	else{
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
+}
+    
+	
