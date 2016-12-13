418cfe82d93a4054ac1d4945a5a2d855ccf20f40
Angelica Zverovich <azverovich@stuy.edu>
2016-12-13 12:31:24 -0500

    Created Driver

---
+++ 04Sorts/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    
+    public static void main (String[] args) {
+	int[] ary = { 2, 5 , 1 , 24 ,21 , 34 , 4,56 , 67 , 61};
+	selectionSort (ary);
+	for (int i: ary) {
+	    System.out.println (i);
+	}
+
+    }
+}
--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -29,15 +29,5 @@ public class Sorts{
 	}
     }
 
-    public static void main (String[] args) {
-	int[] ary = { 2, 5 , 1 , 24 ,21 , 34 , 4,56 , 67 , 61};
-	selectionSort (ary);
-	for (int i: ary) {
-	    System.out.println (i);
-	}
-
-    }
-	
-    
 
 }
